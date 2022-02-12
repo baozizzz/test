752f5aa18168ca3cbff8e42a4cac66e8920e729b
baozizzz <99537423+baozizzz@users.noreply.github.com>
2022-02-12 21:51:06 +0800

    编写录入代码
    第一次编写录入代码

--- java1.java
+++ java1.java
@@ -1,2 +1,17 @@
-<java>
-<1.java>
+import java.util.Scanner;
+// 导包
+public class java1.java {
+// 创建类
+public static void main(Spring[] args) {
+// 入口
+  Scanner sc = new Scanner();
+  // 创建
+  int a = sc.nextInt();
+  //
+  System.out.println(a);
+  // 打印
+}
+}
+
+
+
